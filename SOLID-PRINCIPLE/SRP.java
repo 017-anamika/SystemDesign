86808665b4e4db0d76643f9390d590c1aed2052b
Anamika Verma <146219832+017-anamika@users.noreply.github.com>
2025-09-14 23:04:38 +0530

    SingleRepositoryPattern

---
+++ SOLID-PRINCIPLE/SRP.java
@@ -0,0 +1,24 @@
+class Book {
+    String title, author;
+    Book(String t, String a) { title = t; author = a; }
+}
+
+class BookRepository {
+    void save(Book b) {
+        System.out.println("Saving book: " + b.title);
+    }
+}
+
+class BookPrinter {
+    void print(Book b) {
+        System.out.println("Book: " + b.title + " by " + b.author);
+    }
+}
+
+public class SRPDemo {
+    public static void main(String[] args) {
+        Book b = new Book("Java Basics", "Anamika");
+        new BookRepository().save(b);
+        new BookPrinter().print(b);
+    }
+}
